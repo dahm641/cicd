b075775f6bc83084932f3a782d4ae9f2edce4dc5
dahm641 <danial646@hotmail.co.uk>
2024-05-13 13:19:40 +0100

    test

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # be careful of these keys, they will go out of date
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
-echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+# echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install -y mongodb-org
+
 
 # sudo apt-get install mongodb-org=3.2.20 -y
-sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+# sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
 # remoe the default .conf and replace with our configuration
 # sudo rm /etc/mongod.conf
