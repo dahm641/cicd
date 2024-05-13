9f8a8a0be99e3613c8bfdcda9afabec7a53366e3
dahm641 <danial646@hotmail.co.uk>
2024-05-13 16:32:57 +0100

    test

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -4,7 +4,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA3129
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get install mongodb-org=3.2.20 -y
-# sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 sudo apt-get update -y
 sudo apt-get upgrade -y
 # remoe the default .conf and replace with our configuration
@@ -14,5 +14,6 @@ sudo apt-get upgrade -y
 sudo sed -i 's/^\(\s*\)bindIp: .*/\1bindIp: 0.0.0.0/' /etc/mongod.conf
 # if mongo is is set up correctly these will be successful
 
-sudo systemctl restart mongod
+
 sudo systemctl enable mongod
+sudo systemctl restart mongod
